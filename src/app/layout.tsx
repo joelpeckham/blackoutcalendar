a1caee1eb75d010ebd7b700e133e8ef762126b9c
Joel Peckham <mail@jpeckham.com>
2024-03-05 11:39:12 -0700

    Add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "~/styles/globals.css";
 
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
   subsets: ["latin"],
